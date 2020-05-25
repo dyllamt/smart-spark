d05f237d10ac095de167b5ea540f8ad419312787
dyllamt <maxwell.dylla@gmail.com>
2020-05-25 16:26:32 -0700

    define pdf and log-likelihood for Weibull distribution
    test coverage:
    - tests pdf and log-likelihood with the exponential limit of the Weibull distribution

---
+++ src/smart_spark/entity/model/weibull_model.py
@@ -0,0 +1,61 @@
+"""Implementation of weibull states.
+"""
+import math
+from pyspark.rdd import RDD
+
+from .base_model import BaseModel
+
+
+class WeibullModel(BaseModel):
+    """Two-parameter Weibull distribution of a single variable.
+
+    See the standard parameterization at https://en.wikipedia.org/wiki/Weibull_distribution.
+
+    Parameters
+    ----------
+    shape: float
+        The shape parameter (k).
+    scale: float
+        The scale parameter (lambda).
+
+    Attributes
+    ----------
+    parameters: Dict[str, float]
+        The named-parameters for this model.
+    """
+
+    def __init__(self, shape: float, scale: float):
+        parameters = {"shape": shape, "scale": scale}
+        super().__init__(parameters)
+
+    def pdf(self, x: float) -> float:
+        """The probability density function over the single variable.
+
+        Parameters
+        ----------
+        x: float
+            The single variable.
+
+        Returns
+        -------
+        float
+            The probability density.
+        """
+        shape = self.parameters["shape"]
+        scale = self.parameters["scale"]
+        return (shape / scale) * ((x / scale)**(shape - 1)) * math.exp(-((x / scale)**shape))
+
+    def log_likelihood(self, rdd: RDD) -> float:
+        """Returns the log-likelihood that this model generated the given data distribution.
+
+        Parameters
+        ----------
+        rdd: RDD
+            Elements are assumed to be floats.
+
+        Returns
+        -------
+        float
+            The log-likelihood that this model generated the given data distribution.
+        """
+        return rdd.map(lambda x: math.log(self.pdf(x))).sum()
---
+++ tests/entity/model/test_weibull_model.py
@@ -0,0 +1,30 @@
+import unittest
+import math
+
+from pyspark import SparkContext
+
+from smart_spark.entity.model.weibull_model import WeibullModel
+
+
+class TestWeibullModel(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls._model = WeibullModel(  # sets-up exponential distribution to test on
+            shape=1.,
+            scale=1.
+        )
+
+        sc = SparkContext("local", "test data")  # sets-up data to test on
+        cls._data = sc.parallelize(
+            [0., 0., 0.]
+        )
+
+    def test_pdf(self):
+
+        self.assertEqual(self._model.pdf(0), 1)
+        self.assertAlmostEqual(self._model.pdf(math.e), 0.0659, 3)
+
+    def test_log_likelihood(self):
+
+        self.assertEqual(self._model.log_likelihood(self._data), 0)
